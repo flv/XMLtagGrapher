97f0a1d078179b0489496170c358acd699c5f060
FLV <flv@n0m.fr>
2010-05-06 17:22:23 +0200

    Ajout d'un début d'algo

---
+++ essai.php
@@ -0,0 +1,17 @@
+<?php
+
+
+// Chargement du XML TODO
+
+
+// Remplissage des listes FIXME
+
+//for item in xml->items
+  //for tag in item->tags
+    //$listName = $tag->name;
+    //$Listes as list of list of items...
+    //add item to $Listes["$listName"]
+    //
+
+
+// 
